7b1f96b3316756d3fded3bebafbc5987ec480f7b
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-18 17:10:11 -0500

    Components scaffolded

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react"
 
+import PostContainer from "./components/PostContainer"
+import { posts } from "./data/dummy-data"
+
 class App extends Component {
+  state = {
+    posts
+  }
+
   render() {
-    return <main />
+    const { posts } = this.state
+    return (
+      <>
+        <header>
+          <h1>Instagram</h1>
+        </header>
+        <main>
+          {posts.map(post => (
+            <PostContainer {...post} />
+          ))}
+        </main>
+      </>
+    )
   }
 }
 
