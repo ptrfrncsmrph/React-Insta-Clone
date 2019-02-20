4407145b943465ac215ca803228bf310674ddbbb
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-20 18:30:29 -0500

    Refactors a lot a lot of things

--- instagram/src/App.tsx
+++ instagram/src/App.tsx
@@ -1,23 +1,7 @@
-import React, { useState, FormEvent } from "react"
+// import React, { useState, FormEvent } from "react"
 import withAuth from "./authentication/withAuth"
 import PostsPage from "./components/PostsPage"
 import LoginPage from "./components/LoginPage"
 import "./App.scss"
 
-import { Option, some, none, fromNullable } from "fp-ts/lib/Option"
-
-const App = () => {
-  const _user: Option<string> = fromNullable(localStorage.getItem("user"))
-  const [user, setUser] = useState(_user)
-  const handleAuthChange = (e: FormEvent, username: string) => {
-    e.preventDefault()
-    setUser(some(username))
-    localStorage.setItem("user", username)
-  }
-  return user.fold(
-    <LoginPage handleAuthChange={handleAuthChange} />,
-    username => <PostsPage username={username} />
-  )
-}
-
-export default withAuth(App)
+export default withAuth(LoginPage)(PostsPage)
--- instagram/src/authentication/withAuth.tsx
+++ instagram/src/authentication/withAuth.tsx
@@ -1,9 +1,41 @@
-import React, { Component, ComponentType } from "react"
+import React, { Component, ComponentType, FormEvent } from "react"
+import { Option, some, none, fromNullable } from "fp-ts/lib/Option"
 
-const withAuth = (App: ComponentType) =>
+interface LoginPageProps {
+  handleAuthChange: (e: FormEvent<Element>, username: string) => void
+}
+
+interface PostsPageProps {
+  username: string
+}
+
+const withAuth = (LoginPage: ComponentType<LoginPageProps>) => (
+  PostsPage: ComponentType<PostsPageProps>
+) =>
   class extends Component {
+    state = {
+      username: none
+    }
+
+    componentDidMount() {
+      const username: Option<string> = fromNullable(
+        localStorage.getItem("username")
+      )
+      this.setState(() => ({
+        username
+      }))
+    }
+
+    handleAuthChange = (e: FormEvent, username: string) => {
+      e.preventDefault()
+      this.setState(() => ({ username: some(username) }))
+      localStorage.setItem("username", username)
+    }
     render() {
-      return <App />
+      return this.state.username.fold(
+        <LoginPage handleAuthChange={this.handleAuthChange} />,
+        username => <PostsPage username={username} />
+      )
     }
   }
 
